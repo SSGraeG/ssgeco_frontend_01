d6011f45a7fe335af5eebcd4eaccb62b9f8bf5a4
hwan515 <stophwan515@gmail.com>
2023-12-19 18:21:05 +0900

    S3-66

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -4,8 +4,31 @@ import Logo from '../components/Logo';
 import Header from '../components/Header';
 import Paragraph from '../components/Paragraph';
 import Button from '../components/Button';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Dashboard({ navigation }) {
+  const handleLogout = async () => {
+    try {
+      const token = await AsyncStorage.getItem('Token');
+      const response = await fetch('http://172.20.132.180:5000/logout', {
+        method: 'GET',
+        headers: {'x-access-token': token}
+      });
+
+      if (response.status === 200) {
+        await AsyncStorage.removeItem('Token');
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'StartScreen' }]
+        });
+      } else {
+        console.error('Logout failed:', response.status);
+      }
+    } catch (error) {
+      console.error('Logout Error:', error);
+    }
+  };
+
   return (
     <Background>
       <Logo />
@@ -21,12 +44,7 @@ export default function Dashboard({ navigation }) {
       </Button>
       <Button
         mode="outlined"
-        onPress={() =>
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'StartScreen' }],
-          })
-        }
+        onPress={handleLogout}
       >
         로그아웃
       </Button>
