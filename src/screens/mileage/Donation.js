d9acd4c6351e4fb42333fd22c06864d67a24056a
윤규희 <ojinga0519@naver.com>
2023-12-27 17:13:28 +0900

    S3-92

--- src/screens/mileage/Donation.js
+++ src/screens/mileage/Donation.js
@@ -1,32 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import Background from '../../components/Background';
 import Logo from '../../components/Logo';
 import Header from '../../components/Header';
 import BackButton from '../../components/BackButton';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Donation = ({ navigation }) => {
-  const [currentMileage, setCurrentMileage] = useState(5000);
-
-  const coupons = [
-    { id: 1, name: '1000원 연탄 기부', mileageRequired: 1000 },
-    { id: 2, name: '2000원 동물 보호', mileageRequired: 2000 },
-    { id: 3, name: '3000원 행복 나눔 식권', mileageRequired: 3000 },
-  ];
-
-  const handleExchange = (coupon) => {
-    // 마일리지가 쿠폰에 필요한 마일리지보다 많거나 같으면 교환
-    if (currentMileage >= coupon.mileageRequired) {
-      console.log(`교환: ${coupon.name}`);
-      setCurrentMileage(currentMileage - coupon.mileageRequired);
-      // 쿠폰 교환 로직 추가
-    } else {
-      // 마일리지가 부족한 경우
-      console.log('마일리지가 부족합니다.');
+  const [currentMileage, setCurrentMileage] = useState();
+  const [coupons, setCoupons] = useState([]);
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+  
+  const handleExchange = async (coupon) => {
+    try {
+      const token = await AsyncStorage.getItem('Token');
+      const response = await fetch(`${apiUrl}/donation_use`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': token,
+        },
+        body: JSON.stringify({ coupon_id: coupon.id }), // 해당 쿠폰의 ID 전송
+      });
+  
+      if (response.status === 200) {
+        const newMileageResponse = await fetch(`${apiUrl}/get_user_mileage`, {
+          headers: { 'x-access-token': token }
+        });
+  
+        if (newMileageResponse.status === 200) {
+          const data = await newMileageResponse.json();
+          setCurrentMileage(data.mileage);
+          alert(`쿠폰 전환 성공! 현재 잔액: ${data.mileage}점`);
+        } else if (newMileageResponse.status === 500) {
+          throw new Error('마일리지 업데이트 중 에러가 발생했습니다');
+        } else {
+          throw new Error('서버 오류');
+        }
+      } else if (response.status === 500) {
+        throw new Error('요청 중 에러가 발생했습니다');
+      } else {
+        throw new Error('서버 오류');
+      }
+    } catch (error) {
+        console.error('쿠폰 사용 중 오류 발생:', error);
+        alert(`쿠폰 사용 중 오류 발생`);
     }
   };
+  
+  
+  useEffect(() => {
+    fetch(`${apiUrl}/donation_list`)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else if (response.status === 500) {
+          throw new Error('요청중 에러가 발생');
+        } else {
+          throw new Error('서버 오류');
+        }
+      })
+      .then(data => {
+        if (Array.isArray(data.coupon)) {
+          setCoupons(data.coupon);
+        } else {
+          setCoupons([]);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching coupons:', error);
+        setCoupons([]);
+      });
+  }, []);  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await AsyncStorage.getItem('Token');
+        const response = await fetch(`${apiUrl}/get_user_mileage`, {
+          headers: { 'x-access-token': token }
+        });
+  
+        if (response.status === 200) {
+          const data = await response.json();
+          setCurrentMileage(data.mileage);
+        } else if (response.status === 500) {
+          throw new Error('요청 중 에러가 발생했습니다');
+        } else {
+          throw new Error('서버 오류');
+        }
+      } catch (error) {
+        console.error('사용자 마일리지를 가져오는 중 오류 발생:', error);
+        setCurrentMileage(0);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
 
   return (
     <Background>
@@ -36,22 +108,26 @@ const Donation = ({ navigation }) => {
       <Text style={styles.mileageText}>현재 마일리지: {currentMileage}점</Text>
 
       <ScrollView style={styles.scrollView}>
-      {coupons.map((coupon) => (
-        <Card key={coupon.id}  containerStyle={styles.cardStyle}>
-          <Card.Title>{coupon.name}</Card.Title>
-          <Card.Divider />
-          <Text style={styles.couponText}>
-            필요 마일리지: {coupon.mileageRequired}
-          </Text>
-          <TouchableOpacity
-            onPress={() => handleExchange(coupon)}
-            disabled={currentMileage < coupon.mileageRequired}
-            style={currentMileage >= coupon.mileageRequired ? styles.exchangeButton : styles.disabledButton}
-          >
-            <Text style={styles.exchangeButtonText}>교환</Text>
-          </TouchableOpacity>
-        </Card>
-      ))}
+      {coupons.length > 0 ? (
+        coupons.map((coupon) => (
+          <Card key={coupon.id} containerStyle={styles.cardStyle}>
+            <Card.Title>{coupon.name}</Card.Title>
+            <Card.Divider />
+            <Text style={styles.couponText}>
+              필요 마일리지: {coupon.usepoint}
+            </Text>
+            <TouchableOpacity
+              onPress={() => handleExchange(coupon)}
+              disabled={currentMileage < coupon.usepoint}
+              style={currentMileage >= coupon.usepoint ? styles.exchangeButton : styles.disabledButton}
+            >
+              <Text style={styles.exchangeButtonText}>교환</Text>
+            </TouchableOpacity>
+          </Card>
+        ))
+      ) : (
+        <Text style={styles.noCouponsText}>사용 가능한 쿠폰이 없습니다.</Text>
+      )}
       </ScrollView>
     </Background>
   );
@@ -91,6 +167,12 @@ const styles = StyleSheet.create({
   exchangeButtonText: {
     color: 'white',
     fontWeight: 'bold',
+  },
+  noCouponsText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
   },
 });
 
